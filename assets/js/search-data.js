a9c61b03bd05b45dfba9688de4e2ecb8c7233315
bqzhu14 <baiqingzhu29@gmail.com>
2025-12-29 01:44:43 +0000

    Deploying to gh-pages from @ bqzhu14/bqzhu14.github.io@003e136c2830aa68b7e5934ae5bd41eb879d458c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,8 +10,8 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          title: "Teaching",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
